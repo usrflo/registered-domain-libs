e2f5ed12c91254cdc5396f79dbb8163e8239d5e7
Aaron Hurt <ahurt@anbcs.com>
2014-09-30 00:16:42 -0500

    add class wrapper

--- PHP/test-regDomain.sh
+++ PHP/test-regDomain.sh
@@ -1,7 +1,9 @@
 FQDNS="registered.com sub.registered.com parliament.uk sub.registered.valid.uk registered.somedom.kyoto.jp invalid-fqdn org academy.museum sub.academy.museum subsub.sub.academy.museum sub.nic.pa registered.sb sub.registered.sb subsub.registered.something.zw subsub.registered.9.bg registered.co.bi sub.registered.bi subsub.registered.ee s.tld -test.tld g%d.t/l"
 
 echo '### INPUT ###';
-echo $FQDNS | sed 's/ /\n/g';
-
+echo
+echo $FQDNS | sed 's/\ /\'$'\n/g';
+echo
 echo '### OUTPUT ###';
 php test-regDomain.php $FQDNS
+echo
